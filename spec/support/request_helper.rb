8cafa8d6c184fdbcce73bd682ed1d41120b40f1a
Daniel Vartanov <dan@vartanov.net>
2015-01-07 14:01:35 +0000

    Extract some useful methods to rspec helper

--- spec/support/request_helper.rb
+++ spec/support/request_helper.rb
@@ -3,4 +3,12 @@ module RequestHelper
   def make_async_request(request_url)
     Thread.new { HTTP.via('localhost', 2048).get(request_url) }
   end
+
+  def request_url(key)
+    "http://localhost:4096/v1.0/product.count.json?api_key=s3krit&store_key=#{key}"
+  end
+
+  def request_to_random_store
+    request_url rand
+  end
 end
